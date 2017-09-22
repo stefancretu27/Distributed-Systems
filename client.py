0925c46db123f42d43deb108e928e6abda6a4373
Frans Simanjuntak <franzteve@gmail.com>
2017-09-22 13:31:13 +0200

    Integrated the code with JSON Messaging Format

--- client.py
+++ client.py
@@ -4,6 +4,9 @@ import sys
 import socket
 #
 import select
+#import classes
+from MessageUtil import MessageUtil
+from Enum import MessageType,SenderType,MessageContent
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = int (sys.argv[1])
@@ -11,8 +14,8 @@ rec_msg_buffer_size = 2048
 
 #open socket on same IP (localhost) and using same port
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-connection_message = UDP_IP + UDP_IP
-client_socket.sendto(connection_message, (UDP_IP, UDP_PORT))
+# connection_message = UDP_IP + UDP_IP
+client_socket.sendto(MessageUtil.constructMessage("",SenderType.CLIENT,MessageType.JOINROOM,""), (UDP_IP, UDP_PORT))
 
 sys.stdout.write('[Me:] '); sys.stdout.flush()
 
@@ -28,28 +31,34 @@ while True:
 		#if client socket has data =>server sent a message
 		if socket == client_socket:
 			server_message, server_address = socket.recvfrom(rec_msg_buffer_size)
+			sender_id, sender_type, message_type, message_content = MessageUtil.extractMessage(server_message)
 			if not server_message:
 				print ('disconnected from server')
 				#sys.exit()
 			else:
-				sys.stdout.write('\n'); sys.stdout.flush()
-				sys.stdout.write('[Received message:] '); sys.stdout.flush()
-				sys.stdout.write(server_message)
-				#sys.stdout.write('\n'); sys.stdout.flush()
-				sys.stdout.write('[Me:] '); sys.stdout.flush()
+				if (sender_type == SenderType.SERVER):
+					sys.stdout.write("%s \n"%(message_content))
+				else:
+					# sys.stdout.write('\n'); sys.stdout.flush()
+					# sys.stdout.write('[Received message:] '); sys.stdout.flush()
+					# sys.stdout.write(message_content)
+					#sys.stdout.write('\n'); sys.stdout.flush()
+					sys.stdout.write("[%s] %s"%(sender_id, message_content))
+				sys.stdout.write('[Me] '); sys.stdout.flush()
 		#stdin has data => user wrote a message
 		else:
 			#send message to server. Even if #q is typed, firstly inform the server
 			try:
-				message_from_client = sys.stdin.readline()
-				client_socket.sendto(message_from_client, (UDP_IP, UDP_PORT))
-				#if the client decided to quit
-				if message_from_client.strip() == '~q':
+				client_message = sys.stdin.readline()
+				if (client_message.strip() == MessageContent.QUIT):
+					client_socket.sendto(MessageUtil.constructMessage("", SenderType.CLIENT, MessageType.LEFTROOM, client_message), (UDP_IP, UDP_PORT))
 					#client_socket.close()
 					sys.exit();
 				else:
-					sys.stdout.write('[Me:] '); sys.stdout.flush()
+					client_socket.sendto(MessageUtil.constructMessage("", SenderType.CLIENT, MessageType.NORMALCHAT, client_message), (UDP_IP, UDP_PORT))
+					sys.stdout.write('[Me:] ');
+					sys.stdout.flush()
 			except:
 				print ('The message could not be sent. The socket will close. Type <q> to exit the application')
-				client_socket.sendto('#q', (UDP_IP, UDP_PORT))
+				client_socket.sendto(MessageUtil.constructMessage("", SenderType.CLIENT, MessageType.LEFTROOM, client_message), (UDP_IP, UDP_PORT))
 				client_socket.close()
--- server.py
+++ server.py
@@ -6,8 +6,11 @@ import sys
 #import socket package
 import socket
 import time
+#import classes
 from ClientModel import ClientModel
 from UDPServerModel import UDPServerModel
+from MessageUtil import MessageUtil
+from Enum import MessageType,SenderType
 
 #set message buffer size
 message_buffer_size = 2048
@@ -15,6 +18,7 @@ message_buffer_size = 2048
 this_server = UDPServerModel('127.0.0.1', int (sys.argv[1]))
 #open server socket
 this_server.openSocket()
+print('Server is starting up on %s port %s' % (this_server.ip, this_server.port))
 
 #store all connected clients in a global list	
 list_of_clients = list()
@@ -40,47 +44,66 @@ def findClientInList(this_client):
 			return client
 
 #send a message to all connected clients but to the one that sent it
-def multicastMessage(this_client):
+def multicastMessage(this_client, message_type):
 	#this_server.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+	multicast_sendertype = SenderType.CLIENT
+	multicast_senderid = "%s:%s"%(this_client.address[0], this_client.address[1])
+	multicast_message = this_client.message
+	if (message_type == MessageType.LEFTROOM or message_type == MessageType.JOINROOM):
+		if (message_type == MessageType.JOINROOM):
+			multicast_message = '************************ client %s:%s joined the room ********************************' %(this_client.address[0],this_client.address[1])
+		else:
+			multicast_message = '************************ client %s:%s left the room ****************************' %(this_client.address[0],this_client.address[1])
+		multicast_sendertype = SenderType.SERVER
+		multicast_senderid = "%s:%s"%(this_server.ip, this_server.port)
+
 	for client in list_of_clients:
 		if client != this_client:
-			this_server.socket.sendto(this_client.message, client.address)	 	
+			# this_server.socket.sendto(this_client.message, client.address)
+			this_server.socket.sendto(MessageUtil.constructMessage(multicast_senderid, multicast_sendertype, message_type, multicast_message), client.address)
 
 while True:
 	#Each time a message is received, create a temporary client with the received attributes(message + address)
 	#Recvfrom takes as input message buffer size = the maximum length for the received message
 	#it outputs a pair: first is the data = the message; the second is the client's socket address
 	temp_client = ClientModel(None, None)
-	temp_client.message, temp_client.address = this_server.socket.recvfrom(message_buffer_size)
+	# temp_client.message, temp_client.address = this_server.socket.recvfrom(message_buffer_size)
+	temporary_message, temp_client.address= this_server.socket.recvfrom(message_buffer_size)
+	sender_id, sender_type, message_type, message_content = MessageUtil.extractMessage(temporary_message)
 
+	temp_client.message = message_content
 	client_ip, client_port = temp_client.address
 
 	#new client has requested to join the conversation
-	if str(temp_client.message) == str(client_ip)+str(client_ip):
-		#add to list of connected clients
-		temp_client.message = ''
-		list_of_clients.append(temp_client)
+	if (sender_type == SenderType.CLIENT):
+		if (message_type == MessageType.JOINROOM):
+			temp_client.message = ''
+			list_of_clients.append(temp_client)
 
-		#show the entire list of available connections
-		print ('A new client has joined. The current logged in clients are:')
-		for client in list_of_clients:
-			print (client.address)
-	else:
-		#find this client's attributes in list of connected clients
-		existing_client = findClientInList(temp_client)
-		#update message for the existing_client instance
-		if existing_client is not None:
-			existing_client.setMessage(temp_client.message)
+			#show the entire list of available connections
+			print ('A new client has joined. The current logged in clients are:')
+			for client in list_of_clients:
+				print (client.address)
 
-			#if the client exited, the client application sends a 'quit' message. Remove this client from the client list
-			if temp_client.message.strip() == '~q':
-				list_of_clients = disconnectClient(list_of_clients, existing_client)
-				print ('Client', existing_client.address ,'has left. The current logged in clients are:')
-				showConnectedClients()
-			else:
-				#overwrite his message in list, as only the last message matters. 
-				for client in list_of_clients:
-					if client == existing_client:
-						client.setMessage(existing_client.message)
-				#send message to the other connected clients
-				multicastMessage(existing_client)
+			#send message to the other connected clients
+			multicastMessage(temp_client, MessageType.JOINROOM)
+		else:
+			#find this client's attributes in list of connected clients
+			existing_client = findClientInList(temp_client)
+			#update message for the existing_client instance
+			if existing_client is not None:
+				existing_client.setMessage(temp_client.message)
+
+				#if the client exited, the client application sends a 'quit' message. Remove this client from the client list
+				if (message_type == MessageType.LEFTROOM):
+					multicastMessage(existing_client, MessageType.LEFTROOM)
+					list_of_clients = disconnectClient(list_of_clients, existing_client)
+					print ('Client', existing_client.address ,'has left. The current logged in clients are:')
+					showConnectedClients()
+				else:
+					#overwrite his message in list, as only the last message matters.
+					for client in list_of_clients:
+						if client == existing_client:
+							client.setMessage(existing_client.message)
+					#send message to the other connected clients
+					multicastMessage(existing_client, MessageType.NORMALCHAT)
